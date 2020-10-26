e5a01faa6c4f4411581da2da9da351c469b590ed
Brandon <bbates377@gmail.com>
2020-10-26 12:26:44 -0500

    continue work

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,28 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navigation id='nav'/>
+    <img id='profile' alt="Brandon Bates" src="./assets/profile.jpeg">
+    <p id='summary'>Full Stack Developer specializing in Javascript and adept at both front- and back-end implementation and deployment. Actively searches for elegant, efficient methods for solving problems at hand. Exhibits leadership, confidence in ability, and proficiency in work. Willing and eager to learn new technologies, existing frameworks, and improving thereupon.</p>
+    <Projects id='list' />
+    <Footer id="footer" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navigation from './components/navigation.vue'
+import Projects from './components/projects.vue'
+import Footer from './components/footer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navigation,
+    Projects,
+    Footer
   }
 }
 </script>
 
 <style>
+#nav {
+  background-color: #3d85c6;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding-bottom: 10px;
+  background-color: #cfe0fc;
+}
+#summary {
+  margin: 50px;
+}
+#profile {
+  height: 200px;
+  width: auto;
+  border-radius: 100%;
+  border: 3px solid blanchedalmond;
+  background-color: #3d85c6;
+  margin-top: 10px;
 }
 </style>
